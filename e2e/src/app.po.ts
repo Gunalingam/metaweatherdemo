280072ab66822fd9441b197567451046036cd61c
Gunalingam <guna.lingam@gmail.com>
2019-08-29 01:25:12 +0530

    Fix E2E issue

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    return element(by.tagName('mat-toolbar-row')).getText() as Promise<string>;
   }
 }
